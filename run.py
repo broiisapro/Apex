99728c217bbe6c9b13272985a8d07cddb05a466d
Moksh <moksh.siruvani@gmail.com>
2025-05-12 21:56:06 -0400

    run.py file :wq :wq :wq :wq

---
+++ run.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+
+# Set image size to match training
+IMG_SIZE = (224, 224)
+
+# Load your model
+model = tf.keras.models.load_model("spectro_model.h5")
+
+# Define class names in order
+class_names = ["carbon", "helium", "hydrogen", "nitrogen", "oxygen"]
+
+# Load and preprocess the image
+img_path = 'test_imgs/test_5.jpg'
+image = tf.keras.preprocessing.image.load_img(img_path, target_size=IMG_SIZE)
+img_array = tf.keras.preprocessing.image.img_to_array(image) / 255.0
+img_array = np.expand_dims(img_array, axis=0)
+
+# Make prediction
+pred = model.predict(img_array)[0]
+thresholded = [class_names[i] for i, val in enumerate(pred) if val > 0.95]
+print("Detected elements:", thresholded)
